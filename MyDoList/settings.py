f413d8f620ee9765b6e8fc55ea61229bd4ab88a3
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-01 07:45:59 +0300

    added AUTH_USER_MODEL

--- MyDoList/settings.py
+++ MyDoList/settings.py
@@ -146,7 +146,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
-# AUTH_USER_MODEL = "users.WorkerUser"
+AUTH_USER_MODEL = "tasks.User"
 
 LOGOUT_REDIRECT_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
