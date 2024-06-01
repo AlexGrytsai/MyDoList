13b4c149a1c0f6204fad54c4c5c4cbd256f79216
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-01 07:54:01 +0300

    wrote all models for project

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Tag"
+        verbose_name_plural = "Tags"
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    description = models.TextField(max_length=255, null=True, blank=True)
+    at_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag, null=True, blank=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
