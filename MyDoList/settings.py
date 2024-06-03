5961e7bab89790735282f1ef135deea951cb25ed
Alex Grytsai <a.grytsai1987@gmail.com>
2024-06-03 10:33:14 +0300

    added load_dotenv() in settings.py

--- MyDoList/settings.py
+++ MyDoList/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +44,6 @@ INSTALLED_APPS = [
     "bootstrap4",
     "crispy_forms",
     "crispy_bootstrap4",
-    "tasks"
 ]
 
 MIDDLEWARE = [
